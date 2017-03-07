c0842ebfa0f991117682b094b2c9e70181f6c4f4
mavramen <maya.avramenko@gmail.com>
2017-03-07 11:52:35 -0800

    3-7-17

--- public_html/assets/js/loadHeader.js
+++
@@ -1,8 +0,0 @@
-function addToHeader() {
-    var doc = document.querySelector('link[rel="import"]').import;
-    var div = doc.querySelector('.header-includes');
-
-    for (var child = div.firstChild; child !== null; child = child.nextSibling) {
-        document.head.appendChild(child.cloneNode(true));
-    }
-}
