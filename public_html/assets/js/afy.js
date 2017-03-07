9ba948ecf675a5325d6d195c78bf6c11d3e595e6
mavramen <maya.avramenko@gmail.com>
2017-03-07 10:50:26 -0800

    3-6-2017

--- public_html/assets/js/afy.js
+++ public_html/assets/js/afy.js
@@ -1,23 +1,13 @@
 
 $(document).ready(function () {
 
+    
 
-    /* CSS */
-    /*$("head").append('<title>Alice Fong Yu Alternative School</title>')
-     .append('<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Montserrat:400italic,400">')
-     .append('<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Philosopher|Arimo">')
-     .append('<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">')
-     .append('<link rel="stylesheet" href="assets/css/animate.css">')
-     .append('<link rel="stylesheet" href="assets/css/magnific-popup.css">')
-     .append('<link rel="stylesheet" href="assets/flexslider/flexslider.css">')
-     .append('<link rel="stylesheet" href="assets/css/form-elements.css">')
-     .append('<link rel="stylesheet" href="assets/css/style.css">')
-     .append('<link rel="stylesheet" href="assets/css/media-queries.css">');*/
 
 
 
     /* <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->  */
-//$("head").append('<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries --><!-- WARNING: Respond.js does not work if you view the page via file:// --><!--[if lt IE 9]><script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>//<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->');
+    //$("head").append('<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries --><!-- WARNING: Respond.js does not work if you view the page via file:// --><!--[if lt IE 9]><script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>//<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->');
 
     $("#footer-container").load("footer.html");
     $("#nav-container").load("navigation.html", function () {
@@ -31,7 +21,7 @@ $(document).ready(function () {
         });
     });
 
-    
+
 });
 
 jQuery.fn.extend({
